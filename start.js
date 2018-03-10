3313645fdad049050db0b89ec98cfe5f5c8522f6
scOnTz <piyapatiii37@gmail.com>
2018-03-11 03:22:12 +0700

    demo

--- start.js
+++ start.js
@@ -3,13 +3,26 @@ var cors = require('cors');
 var request = require('request');
 var app = express();
 var bodyParser = require('body-parser');
-
+var cool = require('cool-ascii-faces');
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+    response.send(cool());
+});
 app.post('/getData', function (req, res) {
     console.log(JSON.stringify(req.body));
     request(req.body.url, function (error, response, body) {
